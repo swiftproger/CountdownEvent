a0b802f5003e1447ca66010e8f7157fca64f7149
swiftproger <swiftapp@mail.ru>
2023-02-01 13:55:44 +0300

    Present data in tableViewCell

--- CountdownEvent/CountdownEvent/ViewModel/AddEventViewModel.swift
+++ CountdownEvent/CountdownEvent/ViewModel/AddEventViewModel.swift
@@ -7,61 +7,36 @@
 
 import Foundation
 
-import UIKit
-
 final class AddEventViewModel {
+    
+    var onUpdate: () -> Void = {}
     
     enum Cell {
         case titleSubtitle(TitleSubtitleCellViewModel)
         case titleImage
     }
     
+    private(set) var cells: [AddEventViewModel.Cell] = []
     
     var coordinator: AddEventCoordinator?
+    
+    func viewDidLoad() {
+        cells = [
+            .titleSubtitle(TitleSubtitleCellViewModel(title: "Событие", subtitle: "", placeholder: "Название события")),
+            .titleSubtitle(TitleSubtitleCellViewModel(title: "Дата", subtitle: "", placeholder: "Добавьте дату"))
+        ]
+        onUpdate()
+    }
     
     func viewDidDisappear() {
         coordinator?.didFinishAddEvent()
     }
-}
-
-final class TitleSubtitleCellViewModel {
-    let title: String
-    private(set) var subtitle: String
-    let placeholder: String
     
-    init(title: String, subtitle: String, placeholder: String) {
-        self.title = title
-        self.subtitle = subtitle
-        self.placeholder = placeholder
-    }
-}
-
-final class TitleSubtitleCell: UITableViewCell {
-    private let titleLabel = UILabel()
-    private let subtitleTextField = UITextField()
-    private let verticalStacView = UIStackView()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        setupViews()
-        setupHierarchy()
-        setupLayout()
+    func numberOfRows() -> Int {
+        return cells.count
     }
     
-    private func setupViews() {
-        
-    }
- 
-    private func setupHierarchy() {
-        
-    }
-    
-    private func setupLayout() {
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    func cell(for indexPath: IndexPath) -> Cell {
+        return cells[indexPath.row]
     }
 }
---
+++ CountdownEvent/CountdownEvent/ViewModel/Cell/TitleSubtitleCellViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TitleSubtitleCellViewModel.swift
+//  CountdownEvent
+//
+//  Created by Александр Новиков on 01.02.2023.
+//
+
+final class TitleSubtitleCellViewModel {
+    let title: String
+    private(set) var subtitle: String
+    let placeholder: String
+    
+    init(title: String, subtitle: String, placeholder: String) {
+        self.title = title
+        self.subtitle = subtitle
+        self.placeholder = placeholder
+    }
+}
