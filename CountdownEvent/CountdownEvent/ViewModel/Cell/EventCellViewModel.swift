b11673172687fac25645eed7e2debd738f5b2aa1
swiftproger <swiftapp@mail.ru>
2023-02-07 00:06:04 +0300

    Add file EventCellViewModel

---
+++ CountdownEvent/CountdownEvent/ViewModel/Cell/EventCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  EventCellViewModel.swift
+//  CountdownEvent
+//
+//  Created by Александр Новиков on 08.02.2023.
+//
+
+import Foundation
+
+struct EventCellViewModel {
+    var yearText: String {
+        "1 год"
+    }
+}
