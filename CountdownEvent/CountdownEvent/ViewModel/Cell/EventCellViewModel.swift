1404dbce18e310d29bf42683245e9e27f888e1e7
swiftproger <swiftapp@mail.ru>
2023-02-08 00:26:05 +0300

    Add EventCell

--- CountdownEvent/CountdownEvent/ViewModel/Cell/EventCellViewModel.swift
+++ CountdownEvent/CountdownEvent/ViewModel/Cell/EventCellViewModel.swift
@@ -6,9 +6,34 @@
 //
 
 import Foundation
+import UIKit
 
 struct EventCellViewModel {
     var yearText: String {
         "1 год"
+    }
+    
+    var monthText: String {
+        "2 месяца"
+    }
+    
+    var weekText: String {
+        "2 недели"
+    }
+    
+    var dayText: String {
+        "1 день"
+    }
+    
+    var dateText: String {
+        "25.12.2002"
+    }
+    
+    var eventName: String {
+        "123"
+    }
+    
+    var backgroundImage: UIImage {
+        UIImage()
     }
 }
---
+++ CountdownEvent/CountdownEvent/Views/EventCell.swift
@@ -0,0 +1,59 @@
+//
+//  EventCell.swift
+//  CountdownEvent
+//
+//  Created by Александр Новиков on 08.02.2023.
+//
+
+import Foundation
+import UIKit
+
+class EventCell: UITableViewCell {
+    
+    private let yearLabel = UILabel()
+    private let monthLabel = UILabel()
+    private let weekLabel = UILabel()
+    private let dayLabel = UILabel()
+    private let dateLabel = UILabel()
+    
+    private let eventNameLabel = UILabel()
+    private let backgroundImage = UIImageView()
+    
+    private let verticalStackView = UIStackView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        [yearLabel, monthLabel, weekLabel, dayLabel, dateLabel, eventNameLabel, backgroundImage, verticalStackView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        [yearLabel, monthLabel, weekLabel, dayLabel, dateLabel].forEach {
+            
+            $0.font = .systemFont(ofSize: 22, weight: .medium)
+            $0.textColor = .white
+        }
+        
+        eventNameLabel.font = .systemFont(ofSize: 28, weight: .medium)
+        verticalStackView.axis = .vertical
+    }
+    
+    private func setupHierarchy() {
+        contentView.addSubview(backgroundImage)
+        contentView.addSubview(verticalStackView)
+        
+        [yearLabel, monthLabel, weekLabel, dayLabel, dateLabel].forEach {
+            verticalStackView.addArrangedSubview($0)
+        }
+    }
+    
+    private func setupLayout() {
+        
+    }
+}
