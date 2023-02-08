68680d2b4f92e1366e449dc62e4d0fe6c1754328
swiftproger <swiftapp@mail.ru>
2023-02-08 15:32:20 +0300

    Save resize image

--- CountdownEvent/CountdownEvent/Managers/CoreDataManager.swift
+++ CountdownEvent/CountdownEvent/Managers/CoreDataManager.swift
@@ -27,7 +27,10 @@ final class CoreDataManager {
     func seveEvent(name: String, date: Date, image: UIImage) {
         let event = Event(context: moc)
         event.setValue(name, forKey: "name")
-        let imageData = image.jpegData(compressionQuality: 1)
+        
+        let resizeImage = image.sameAspectRatio(newHeight: 250)
+        
+        let imageData = resizeImage.jpegData(compressionQuality: 0.5)
         event.setValue(imageData, forKey: "image")
         event.setValue(date, forKey: "date")
         
