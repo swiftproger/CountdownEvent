91392b9d0f6d7c1688419435a1691e94ccbecec5
swiftproger <swiftapp@mail.ru>
2023-02-10 10:55:04 +0300

    Add EditEventViewController + storyBoard

---
+++ CountdownEvent/CountdownEvent/Controllers/EditEventViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditEventViewController.swift
+//  CountdownEvent
+//
+//  Created by Александр Новиков on 10.02.2023.
+//
+
+import UIKit
+
+class EditEventViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
